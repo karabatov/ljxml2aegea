6c09dd9910acf20369ca11c322211235dfb3210f
Yuri Karabatov <karabatov@gmail.com>
2012-12-23 22:38:15 +0400

    Add current music, mood and location import

--- ljxml2aegea.php
+++ ljxml2aegea.php
@@ -90,7 +90,7 @@ function to_ascii($str, $delimiter='-') {
 // LJ tag cleaner
 function ljclean($ljtext)
 {
-    echo "* in ljclean function\n";
+    //echo "* in ljclean function\n";
     $userPA = '/<lj user="(.*?)" ?\/?>/i';
     $commPA = '/<lj comm="(.*?)" ?\/?>/i';
     $namedCutPA = '/<lj-cut +text="(.*?)" ?\/?>/i';
@@ -129,6 +129,16 @@ function xmlparser($file_id,$total_post,$ljuser,$e2tabprefix,$offset,$addtags_id
     }
     $p_addtags=$entry->taglist;
     $p_postid=$entry->ditemid;
+    $p_mood=$entry->props->current_mood;
+    $p_music=$entry->props->current_music;
+    $p_location=$entry->props->current_location;
+    $p_coords=$entry->props->current_coords;
+    echo "p_location\n";
+    print_r($p_location);
+    echo "p_music\n";
+    print_r($p_music);
+    echo "p_coords\n";
+    print_r($p_coords);
 
     // If post has no title, take first 50 characters
     if ($p_subj==''){
@@ -138,6 +148,26 @@ function xmlparser($file_id,$total_post,$ljuser,$e2tabprefix,$offset,$addtags_id
         $p_subj.='...';
         }
 
+    // Append mood, music & location info
+    $p_text.=PHP_EOL.PHP_EOL;
+    // Location
+    if ($p_location){
+        if ($p_coords){
+            $p_loctext=$p_coords . ' (' . $p_location . ')';
+            $p_text.='<strong>Current location:</strong> <a href="http://maps.google.com/maps?q='.urlencode($p_loctext).'">'.$p_location.'</a>'.PHP_EOL;
+        } else {
+            $p_text.='<strong>Current location:</strong> <a href="http://maps.google.com/maps?q='.urlencode($p_location).'">'.$p_location.'</a>'.PHP_EOL;
+        }
+    }
+    // Mood
+    if ($p_mood){
+        $p_text.='<strong>Current mood:</strong> '.$p_mood.PHP_EOL;
+    }
+    // Music
+    if ($p_music){
+        $p_text.='<strong>Current music:</strong> '.$p_music;
+    }
+
     // Comments
     // Store all comments for now
     $total_comm=0;
@@ -145,8 +175,8 @@ function xmlparser($file_id,$total_post,$ljuser,$e2tabprefix,$offset,$addtags_id
         $xml = simplexml_load_file("./".$ljuser."/C-".$total_post);
         $i=1;
         foreach ($xml->comment as $comment){
-            echo "i = $i, total_comm = $total_comm\n";
-            print_r($comment);
+            //echo "i = $i, total_comm = $total_comm\n";
+            //print_r($comment);
             $c_id[$i]=$comment->id;
             $c_author[$i]=$comment->user=='' ? 'anonymous' : $comment->user;
             $c_text[$i]=ljclean($comment->body);
@@ -214,17 +244,17 @@ function xmlparser($file_id,$total_post,$ljuser,$e2tabprefix,$offset,$addtags_id
     // ReplyLastModified
     for ($i=1;$i<=$total_comm;$i++){
         if ($c_text[$i]==''){$c_text[$i]='(deleted comment)';};
-        $c_text[$i] = iconv('UTF-8','Windows-1251',$c_text[$i]);
-        $c_author[$i] = iconv('UTF-8','Windows-1251',$c_author[$i]);
+        $c_text[$i] = iconv('UTF-8','Windows-1251//IGNORE',$c_text[$i]);
+        $c_author[$i] = iconv('UTF-8','Windows-1251//IGNORE',$c_author[$i]);
         // ""
         $c_text[$i]=mysql_escape_string($c_text[$i]);
-        echo "---------\n";
-        echo "i = $i, total_comm = $total_comm\n";
-        echo "c_id = $c_id[$i]\n";
-        echo "c_parentid = $c_parentid[$i]\n";
-        echo "p_postid = $p_postid\n";
-        echo "c_author = $c_author[$i]\n";
-        echo "c_text = $c_text[$i]\n";
+        //echo "---------\n";
+        //echo "i = $i, total_comm = $total_comm\n";
+        //echo "c_id = $c_id[$i]\n";
+        //echo "c_parentid = $c_parentid[$i]\n";
+        //echo "p_postid = $p_postid\n";
+        //echo "c_author = $c_author[$i]\n";
+        //echo "c_text = $c_text[$i]\n";
         if (($c_author[$i]==$ljuser)&&($c_parentid[$i]!='')){
             $c_parentid[$i]=$c_parentid[$i]+100000;
             $query = "INSERT INTO ".$e2tabprefix."Comments (ID,IsReplyVisible,Reply,ReplyStamp,ReplyLastModified) VALUES ($c_parentid[$i],1,'$c_text[$i]','$c_date[$i]','$c_date[$i]') ON DUPLICATE KEY UPDATE IsReplyVisible=1,Reply='$c_text[$i]',ReplyStamp='$c_date[$i]',ReplyLastModified='$c_date[$i]';";
