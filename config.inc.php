0631fbfae2aa51f2bd74061373e7752f2fec0274
Yuri Karabatov <karabatov@gmail.com>
2012-12-23 21:34:40 +0400

    Add tag support.
    1. Add tags from config file to every imported post
    2. Add tags from LJ posts into DB

--- config.inc.php
+++ config.inc.php
@@ -1,11 +1,11 @@
 <?php
-// Config for access to Aegea DB
-$e2server="localhost";
-$e2database="aegea";
-$e2user="root";
-$e2password="";
-$e2tabprefix="e2Blog";
-$ljuser="lj_username";
-$offset="14400";
-$addtags="livejournal"; //one tag only for now! no spaces
+// Config for LiveJournal import script 
+$e2server="localhost";  // MySQL Server hostname or IP
+$e2database="aegea";    // Aegea database name
+$e2user="root";         // MySQL username
+$e2password="";         // MySQL password
+$e2tabprefix="e2Blog";  // Aegea table prefix
+$ljuser="lj_username";  // LiveJournal username for author comment attribution
+$offset="14400";        // Offset from GMT in minutes
+$addtags="livejournal, по-русски,with space, specialchars$@#$"; // Tags to add to imported posts
 ?>
